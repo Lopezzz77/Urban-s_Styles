33f6667186a51bc01a0edd6fb248aa76c3c0dccd
Cesariño <cesargabrielolopez77@gmail.com>
2026-06-09 11:36:24 -0300

    Traer Productos y CARDS

--- Project/Api/views.py
+++ Project/Api/views.py
@@ -1,13 +1,33 @@
 from urllib import request
 from django.shortcuts import render
+
+from Project.Api.models import Producto
 from .forms import FormularioRegistro
 
 def Home(request):
     return render(request, 'index.html')
 
 def Registrar(request):
-   data = {
+    data = {
         'form': FormularioRegistro()
     }
-   
-   return render(request, 'Pages/registrar.html',data)
+    if request.method=="POST":
+        query=FormularioRegistro(data=request.POST,files=request.FILES)
+        if query.is_valid():
+            query.save()
+            data["Mensaje"]="Datos Registrados"
+            
+        else:
+            data['Formulario']="NO SE PUEDE REGISTRAR"
+    return render(request, 'Pages/registrar.html',data)
+
+
+
+
+
+def VerProductos(request):
+    query=Producto.objects.all()
+    data={
+        'Productos':query
+    }
+    return render(request, 'Pages/verProductos.html', data)
